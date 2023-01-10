f1c1f53c42f5dd8edaea534b6af9a5713611c615
Rahul Verma <rv404674@gmail.com>
2023-01-10 15:03:09 +0530

    Code for importing modules

---
+++ notes_app/app.js
@@ -0,0 +1,9 @@
+const fileSystem = require("fs")
+
+// NOTE: to use any library in node, you have to use require to load it
+//  and store it in a variable.
+
+
+fileSystem.writeFileSync("notes.txt", 'This file was created by Node.js');
+// to append to the file
+fileSystem.appendFileSync("notes.txt", " Appended a line");
