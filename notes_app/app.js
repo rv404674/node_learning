9b2caac02c90176839a4a98e9a8f276f59f7e4a1
Rahul Verma <rv404674@gmail.com>
2023-01-10 15:12:24 +0530

    learnt how to load utils func from one file to the other

--- notes_app/app.js
+++ notes_app/app.js
@@ -1,9 +1,11 @@
+const firstName = require("./utils")
 const fileSystem = require("fs")
 
 // NOTE: to use any library in node, you have to use require to load it
 //  and store it in a variable.
 
 
-fileSystem.writeFileSync("notes.txt", 'This file was created by Node.js');
+// fileSystem.writeFileSync("notes.txt", 'This file was created by Node.js');
 // to append to the file
-fileSystem.appendFileSync("notes.txt", " Appended a line");
+// fileSystem.appendFileSync("notes.txt", " Appended a line");
+console.log(firstName);
---
+++ notes_app/utils.js
@@ -0,0 +1,6 @@
+console.log("in Utils.js")
+
+const name = 'Sachin';
+
+// NOTE: you need to write here what you want to be assigned.
+module.exports = name;
