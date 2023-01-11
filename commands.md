2c295fa6db2f71632810a0f01fe2845f0db7f6ee
Rahul Verma <rv404674@gmail.com>
2023-01-11 15:10:04 +0530

    learnt about how to use npm pacakges

---
+++ commands.md
@@ -0,0 +1,16 @@
+# COMMAND
+
+## NPM
+To use npm
+do ```
+npm init```
+from the root dir.
+
+then to install
+```json
+npm install validator
+``
+
+NOTE: It will create two things
+1. a node_modules folder that will has the code of the library.
+2. and add a entry in package.json file.
--- notes_app/app.js
+++ notes_app/app.js
@@ -1,6 +1,7 @@
 // const utils = require("./utils")
 const fileSystem = require("fs")
-const notes = require("./notes");
+const getNotes = require("./notes");
+const validator = require("validator");
 // const helloWorld = require("../helloWorld")
 
 // NOTE: to use any library in node, you have to use require to load it
@@ -8,9 +9,15 @@ const notes = require("./notes");
 
 
 // fileSystem.writeFileSync("notes.txt", 'This file was created by Node.js');
-// to append to the file
+// to append to the fil
 // fileSystem.appendFileSync("notes.txt", " Appended a line");
-console.log(utils.add(1,5));
-console.log(utils.name);
+// console.log(utils.add(1,5));
+// console.log(utils.name);
+//
+// console.log("My Notes are" + " " + notes.getNotes());
 
-console.log("My Notes are" + " " + notes.getNotes());
+const msg = getNotes.getNotes();
+console.log(msg)
+
+console.log(validator.isEmail("rv404674@gmail.com"));
+console.log(validator.isURL("https://io.url"));
