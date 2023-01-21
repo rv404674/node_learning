402c82b269b9d9078e2269c77ffa6a898a9424b7
Rahul Verma <rv404674@gmail.com>
2023-01-21 15:23:14 +0530

    learnt fs async function

--- first_app/app.js
+++ first_app/app.js
@@ -1,5 +1,7 @@
 const logger = require("./logger");
 const path = require("path");
+const os = require("os");
+const fs = require("fs");
 
 function sayHello(name){
     console.log("Hello " + name + " This is my first node program");
@@ -14,3 +16,23 @@ logger.log('Logging this to cloud.');
 // PATH MODULE
 let pathObj = path.parse(__filename);
 console.log(pathObj);
+
+// os module
+let totalMem = os.totalmem();
+let freeMem = os.freemem();
+
+// console.log("Total Memory: " + totalMem);
+// NOTE: use template string
+console.log(`Total Memory: ${totalMem}`);
+console.log(`Free Memory: ${freeMem}`);
+
+
+// FileSystem
+// NOTE: Fs has both sync and non sycn methonds.
+// Always try to use non sync methods.
+fs.readdir('./', function (err, files) {
+    // NOTE: only one will have a value - error or files
+    if(err) console.log('Error', er);
+    else console.log('Result', files);
+});
+
