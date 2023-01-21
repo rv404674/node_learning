35a7b37b3cbb4e36bee590ca8bbf3bf5c95b9b8e
Rahul Verma <rv404674@gmail.com>
2023-01-21 15:07:54 +0530

    learnt about path module

--- first_app/app.js
+++ first_app/app.js
@@ -1,4 +1,5 @@
 const logger = require("./logger");
+const path = require("path");
 
 function sayHello(name){
     console.log("Hello " + name + " This is my first node program");
@@ -9,3 +10,7 @@ sayHello("Rahul");
 // NOTE: Every file in Node is considered as a Module.
 
 logger.log('Logging this to cloud.');
+
+// PATH MODULE
+let pathObj = path.parse(__filename);
+console.log(pathObj);
